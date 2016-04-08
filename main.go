2433a120f9f48df6352eed7fa0706f1921897e87
Vitor Mattos <vitor.mattos@vtex.com.br>
2016-04-07 21:08:02 -0300

    Parses SetupHook

--- main.go
+++ main.go
@@ -1,11 +1,20 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"log"
 
 	"github.com/valyala/fasthttp"
 )
+
+type SetupHook struct {
+	Account   string   `json:"account"`
+	Workspace string   `json:"workspace"`
+	Hash      string   `json:"hash"`
+	Removals  []string `json:"removals"`
+	Additions []string `json:"additions"`
+}
 
 func main() {
 	fasthttp.ListenAndServe(":8080", fastHTTPHandler)
@@ -14,6 +23,12 @@ func main() {
 func fastHTTPHandler(ctx *fasthttp.RequestCtx) {
 	path := string(ctx.Path())
 	method := string(ctx.Method())
+
+	hook := SetupHook{}
+	err := json.Unmarshal(ctx.PostBody(), &hook)
+	if err != nil {
+		panic(err)
+	}
 
 	log.Printf("[%s]: %s %s", ctx.RemoteAddr(), method, path)
 
